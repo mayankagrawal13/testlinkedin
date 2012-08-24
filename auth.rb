34306174c9719c1f3e35d07bb4c2489179eb0b93
Mayank Agrawal <mayank@mayank-u.(none)>
2012-08-24 17:11:02 +0530

    Inserted comment to updated group id

--- auth.rb
+++ auth.rb
@@ -12,12 +12,6 @@ configuration = {:site=> 'https://api.linked.com'}
 consumer = OAuth::Consumer.new(api_key, api_secret, configuration)
 
 @access_token = OAuth::AccessToken.new(consumer, user_token, user_secret)
-=begin
-#response = @access_token.get("http://api.linkedin.com/v1/people/~?format=json")
-response = @access_token.get("http://api.linkedin.com/v1/people/~/group-memberships:(group:(id,name),membership-state)?format=json")
-result = JSON.parse(response.body)
-puts result
-=end
-#response = @access_token.post('http://api.linkedin.com/v1/groups/4588653/posts?format=json', {:title => 'My Post Title', :summary => 'My Post Summary'}, {'Content-Type' => 'application/json', 'x-li-format' => 'json'})
-response = @access_token.post('http://api.linkedin.com/v1/groups/4588653/posts', '<post><title>Post Title</title><summary>my summary</summary></post>', {'Content-Type' => 'application/xml'})
+#Replace {group id} in the url below with the actual numeric group id e.g. 4588653
+response = @access_token.post('http://api.linkedin.com/v1/groups/{group id}/posts', '<post><title>Post Title</title><summary>my summary</summary></post>', {'Content-Type' => 'application/xml'})
 #puts response.body
